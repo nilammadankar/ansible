d565351485b7c956fc9f35c6d097b222efa684cf
nilam <nilam@gmail.com>
2022-07-13 15:42:29 +0530

    seed

--- seed.groovy
+++ seed.groovy
@@ -1,5 +1,15 @@
-job('test1') {
+job('new1') {
     steps {
         shell('echo hello world')
+    }
+}
+job('new2') {
+    steps {
+        shell('echo welcome')
+    }
+}
+job('new3') {
+    steps {
+        shell('echo seed job activity')
     }
 }
